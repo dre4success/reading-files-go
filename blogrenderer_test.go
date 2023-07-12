afe9b18bcb6f0a5ea1713a16f8dc89d3b4721d0c
dre4success <dre4success@live.com>
2023-07-12 07:50:37 +0100

    render html

---
+++ blogrenderer_test.go
@@ -0,0 +1,36 @@
+package blogposts_test
+
+import (
+	"bytes"
+	"github.com/dre4success/blogposts"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogposts.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogposts.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>hello world</h1>
+<p>This is a description</p>
+Tags: <ul><li>go</li><li>tdd</li></ul>`
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+}
